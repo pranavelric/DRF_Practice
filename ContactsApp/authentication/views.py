e9a58f2df3bfe4e29f7da6893bc711a8a2bba1c9
Pranav Choudhary <2018184@iiitdmj.ac.in>
2022-01-01 00:00:26 +0530

    Bugs Fixed

--- ContactsApp/authentication/views.py
+++ ContactsApp/authentication/views.py
@@ -26,8 +26,6 @@ class UserLoginApi(GenericAPIView):
         data = request.data
         username = data.get('username', '')
         password = data.get('password', '')
-        print(username)
-        print(password)
         user = auth.authenticate(username=username, password=password)
         if user:
             auth_token = jwt.encode({'username': user.username}, settings.JWT_SECRET_KEY)
